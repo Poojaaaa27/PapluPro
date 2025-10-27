2218961d8a752d1ad714028ba2c855bfd8238b78
Pooja J <itspjdata2519@gmail.com>
2025-10-27 14:50:57 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2998812442-d3d08",
+  "appId": "1:227742513109:web:2fff0083ac7b48cf2b6f5c",
+  "apiKey": "AIzaSyAPtGaJu_FNYAoNy9Ul3yrJ23Bm7vRDmN8",
+  "authDomain": "studio-2998812442-d3d08.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "227742513109"
+};
