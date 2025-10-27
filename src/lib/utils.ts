8e2547571ad151bff977d601fb6b1bab19af7ac4
Pooja J <itspjdata2519@gmail.com>
2025-10-27 07:53:10 +0000

    if i enter 0 in the points input...it shoulg appear in that particular r

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -24,7 +24,7 @@ export function getStatusString(status: PlayerStatus): string {
             postRoundParts.push('D');
             break;
         case 'Playing':
-            if(status.points !== null && status.points > 0) postRoundParts.push(`${status.points}`);
+            if(status.points !== null) postRoundParts.push(`${status.points}`);
             break;
         case 'Full':
             postRoundParts.push('F');
